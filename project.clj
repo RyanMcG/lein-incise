626e0337a25edacc727868d69fd8d6dab737d1a0
Ryan McGowan <ryan@ryanmcg.com>
2014-01-13 23:04:50 -0800

    Bump version of incise to 0.1.0.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-incise "0.1.0-SNAPSHOT"
+(defproject lein-incise "0.1.0"
   :description "A leiningen plugin for the incise static site generator."
   :url "https://github.com/RyanMcG/lein-incise"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[incise "0.1.0-SNAPSHOT"]]
+  :dependencies [[incise "0.1.0"]]
   :eval-in-leiningen true)
