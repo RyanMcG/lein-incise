e9303d001339071d8da67dcf9460b5724efc6e23
Ryan McGowan <ryan@ryanmcg.com>
2014-01-13 22:32:09 -0800

    Use builtin help.

--- src/leiningen/incise.clj
+++ src/leiningen/incise.clj
@@ -4,12 +4,17 @@
 
 (def default-profile {:dependencies [['incise "0.1.0-SNAPSHOT"]]})
 
-(defn ^:no-project-needed incise
-  "Call incise's main method and execute it in the scope of the project if it
-  exists."
+(defn ^{:no-project-needed true
+        :help-arglists []}
+  incise
   [project & args]
   (let [profile (or (get-in project [:profiles :incise])
                     default-profile)]
     (eval-in-project (merge-profiles project [profile])
                      `(incise.core/-main ~@args)
                      '(require 'incise.core))))
+
+(defn help
+  "Use help built into incise instead of leiningen's."
+  []
+  (incise {} "--help"))
