b8356c41c39477e9b3d9e9488b590c09f0890e29
Ryan McGowan <ryan@ryanmcg.com>
2014-03-06 20:08:07 -0800

    Add pass-through-help meta to incise plugin defn.

--- src/leiningen/incise.clj
+++ src/leiningen/incise.clj
@@ -4,17 +4,10 @@
 
 (def default-profile {:dependencies [['incise "0.2.0"]]})
 
-(defn ^{:no-project-needed true
-        :help-arglists []}
-  incise
+(defn ^:no-project-needed ^:pass-through-help incise
   [project & args]
   (let [profile (or (get-in project [:profiles :incise])
                     default-profile)]
     (eval-in-project (merge-profiles project [profile])
                      `(incise.core/-main ~@args)
                      '(require 'incise.core))))
-
-(defn help
-  "Use help built into incise instead of leiningen's."
-  []
-  (incise {} "--help"))
